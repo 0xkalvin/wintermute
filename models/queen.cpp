259b60f708f06a99176dcc793f0f99be264f57ab
kalvin <kalvinvini1998@gmail.com>
2019-07-10 20:42:48 -0300

    queen move verification added

--- models/queen.cpp
+++ models/queen.cpp
@@ -6,8 +6,11 @@ Queen::Queen(bool white) : Piece(queenSymbol, queenValue, white){}
 Queen::~Queen(){}
 
 void Queen::move(Board *b, int xOrigin, int yOrigin, int xDestination, int yDestination){
-    
+    int xDiff = abs(xOrigin - xDestination);
+    int yDiff = abs(yOrigin - yDestination);
 
+    if(xDiff != yDiff && xOrigin != xDestination && yOrigin != yDestination) throw "Queen cannot move to that square";
+    
     b->matrix[xOrigin][yOrigin] = emptyValue;
     b->matrix[xDestination][yDestination] = this->isWhite() ? this->getValue() : -1*this->getValue();
 }

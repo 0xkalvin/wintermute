ead2edd156bd8badea8f32af0cb1c67995c088e0
kalvin <kalvinvini1998@gmail.com>
2019-07-10 21:25:59 -0300

    king move verification added + pawn fixed

--- models/king.cpp
+++ models/king.cpp
@@ -6,9 +6,12 @@ King::King(bool white) : Piece(kingSymbol, kingValue, white){}
 King::~King(){}
 
 void King::move(Board *b, int xOrigin, int yOrigin, int xDestination, int yDestination){
-    
+    int xDiff = abs(xOrigin - xDestination);
+    int yDiff = abs(yOrigin - yDestination);
 
-
+    if((abs(xDestination - xOrigin) > 1 || abs(yDestination - yOrigin) > 1) || 
+    (xDiff != yDiff && xOrigin != xDestination && yOrigin != yDestination)) 
+        throw "King cannot move to that square";
 
     b->matrix[xOrigin][yOrigin] = emptyValue;
     b->matrix[xDestination][yDestination] = this->isWhite() ? this->getValue() : -1*this->getValue();
--- models/pawn.cpp
+++ models/pawn.cpp
@@ -5,15 +5,19 @@ Pawn::Pawn(bool white) : Piece(pawnSymbol, pawnValue, white){}
 
 Pawn::~Pawn(){}
 
-void Pawn::move(Board *b, int xOrigin, int yOrigin, int xDestination, int yDestination){
-    // check line
-    if(abs(xDestination - xOrigin) > 2 || abs(xDestination - xOrigin) < 0){
-        throw "Pawn cannot move to that square";
-    } 
-    // check column
-    if(abs(yDestination - yOrigin) > 1 || abs(yDestination - yOrigin) < 0 ){
-        throw "Pawn cannot move to that square";
-    } 
+void Pawn::move(Board *b, int xOrigin, int yOrigin, int xDestination, int yDestination){ 
+    if(( xOrigin == 6 && this->isWhite() ) || ( xOrigin == 1 && !this->isWhite()) ){
+        if(abs(xDestination - xOrigin) > 2 || ( abs(yDestination - yOrigin) > 0 && abs(xDestination - xOrigin) == 2 )) 
+            throw "Pawn cannot move to that square";
+        if(abs(xDestination - xOrigin) == 1 && abs(yDestination - yOrigin) > 1)
+            throw "Pawn cannot move to that square";
+    }
+    else{
+        if(abs(xDestination - xOrigin) > 1 || abs(yDestination - yOrigin) > 1)
+            throw "Pawn cannot move to that square";
+    }
+
+
 
     b->matrix[xOrigin][yOrigin] = emptyValue;
     b->matrix[xDestination][yDestination] = this->isWhite() ? this->getValue() : -1*this->getValue(); 

b42802e54d854a86e1a50ab630b8fa8b0da0e3fd
kalvin <kalvinvini1998@gmail.com>
2019-07-11 22:07:02 -0300

    added pawn verification for capturing

--- models/pawn.cpp
+++ models/pawn.cpp
@@ -6,6 +6,9 @@ Pawn::Pawn(bool white) : Piece(pawnSymbol, pawnValue, white, pawnQuantity){}
 Pawn::~Pawn(){}
 
 void Pawn::move(Board *b, int xOrigin, int yOrigin, int xDestination, int yDestination){ 
+
+    cout << "VALOR: " << b->matrix[xDestination][yDestination] <<endl;
+
     if(( xOrigin == 6 && this->isWhite() ) || ( xOrigin == 1 && !this->isWhite()) ){
         if(abs(xDestination - xOrigin) > 2 || ( abs(yDestination - yOrigin) > 0 && abs(xDestination - xOrigin) == 2 )) 
             throw "Pawn cannot move to that square";
@@ -17,7 +20,14 @@ void Pawn::move(Board *b, int xOrigin, int yOrigin, int xDestination, int yDesti
             throw "Pawn cannot move to that square";
     }
 
-
+    // if pawn will not capture a piece, then it cannot move in diagonal
+    if(b->matrix[xDestination][yDestination] == 0 && abs(yDestination - yOrigin) > 0)
+        throw "Pawn cannot move to that square";
+    
+    // Pawn cannot move forward if there`s another piece right in the front of it
+    if(b->matrix[xDestination][yDestination] != 0 && abs(xDestination - xOrigin) == 1 && abs(yDestination - yOrigin) == 0)
+        throw "Pawn cannot move to that square";
+    
 
     b->matrix[xOrigin][yOrigin] = emptyValue;
     b->matrix[xDestination][yDestination] = this->isWhite() ? this->getValue() : -1*this->getValue(); 

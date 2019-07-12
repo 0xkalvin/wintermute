b684b0a19998047528398a4b4799847b1d5183eb
kalvin <kalvinvini1998@gmail.com>
2019-07-11 22:18:14 -0300

    pawn verification for not moving backwards

--- models/pawn.cpp
+++ models/pawn.cpp
@@ -26,8 +26,13 @@ void Pawn::move(Board *b, int xOrigin, int yOrigin, int xDestination, int yDesti
     
     // Pawn cannot move forward if there`s another piece right in the front of it
     if(b->matrix[xDestination][yDestination] != 0 && abs(xDestination - xOrigin) == 1 && abs(yDestination - yOrigin) == 0)
-        throw "Pawn cannot move to that square";
-    
+        throw "Pawn cannot move to that square, there's already a piece in it";
+
+    int backwards = xDestination - xOrigin;
+    if(this->isWhite() && backwards > 0)
+        throw "Pawn cannot move backwards";
+    else if(!this->isWhite() && backwards < 0)
+        throw "Pawn cannot move backwards";
 
     b->matrix[xOrigin][yOrigin] = emptyValue;
     b->matrix[xDestination][yDestination] = this->isWhite() ? this->getValue() : -1*this->getValue(); 
